dd8357983bbd1613ef70f3de02d5148725918e8b
mori-medix <mori@medix-inc.co.jp>
2023-07-10 16:39:09 +0900

    tenant ページ対応

--- base-dir.js
+++ base-dir.js
@@ -9,22 +9,26 @@ export function getBaseDir(type,__dirname) {
     ];
   } else if(type === 'css'){
     baseDir = [
-      {
-        inputPath: `${__dirname}/src/pages/hebel-rooms/search/list_a/index.scss`,
-        outputPath: `${__dirname}/dist/hebel-rooms/common_v2/css/search/style.css`,
-      },
-      {
-        inputPath: `${__dirname}/src/pages/hebel-rooms/lightbox08/index.scss`,
-        outputPath: `${__dirname}/dist/hebel-rooms/lightbox08/css/style.css`,
-      },
-      {
-        inputPath: `${__dirname}/src/pages/hebel-rooms/features/hebelmaison/style.scss`,
-        outputPath: `${__dirname}/dist/hebel-rooms/common_v2/css/features/hebelmaison/style.css`,
-      },
+      // {
+      //   inputPath: `${__dirname}/src/pages/hebel-rooms/search/list_a/index.scss`,
+      //   outputPath: `${__dirname}/dist/hebel-rooms/common_v2/css/search/style.css`,
+      // },
+      // {
+      //   inputPath: `${__dirname}/src/pages/hebel-rooms/lightbox08/index.scss`,
+      //   outputPath: `${__dirname}/dist/hebel-rooms/lightbox08/css/style.css`,
+      // },
+      // {
+      //   inputPath: `${__dirname}/src/pages/hebel-rooms/features/hebelmaison/style.scss`,
+      //   outputPath: `${__dirname}/dist/hebel-rooms/common_v2/css/features/hebelmaison/style.css`,
+      // },
       // {
       //   inputPath: `${__dirname}/src/pages/hebel-rooms/features/hebelmaison/swiper.scss`,
       //   outputPath: `${__dirname}/dist/hebel-rooms/common_v2/css/features/hebelmaison/swiper.css`,
       // },
+      {
+        inputPath: `${__dirname}/src/pages/hebel-rooms/search/detail/index.scss`,
+        outputPath: `${__dirname}/dist/hebel-rooms/css/detail/style.css`,
+      },
     ];
   }
   return baseDir;
