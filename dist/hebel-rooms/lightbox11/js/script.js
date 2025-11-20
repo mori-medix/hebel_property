e6235a4cf83291373b5a42c333ae19b59b68c163
mori-medix <mori@medix-inc.co.jp>
2025-11-20 22:15:37 +0900

    ライトボックス11作成

---
+++ dist/hebel-rooms/lightbox11/js/script.js
@@ -0,0 +1,31 @@
+$(function () {
+  //smoothscroll
+  //anchor
+  $('a[href^="#"]').on('click touch', function () {
+    var target = $(this.hash);
+    if (target) {
+      var targetY = target.offset().top;
+      $('html,body').animate({ scrollTop: targetY }, 500);
+      return false;
+    }
+  });
+  
+  //swiper
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    effect: 'fade',
+    initialSlide: 0,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-btn-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+  })
+});
